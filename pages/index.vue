9f3c4f35e49dc57648c506a967a5dbd10a562dcc
Angel Hernández Tapia <anarqueabrn@gmail.com>
2024-03-29 16:37:19 -0600

    Adición de comentarios

--- pages/index.vue
+++ pages/index.vue
@@ -37,36 +37,58 @@ export default {
   },
 
   computed: {
+    /**
+     * retorna la lista de tareas almacenada en el store o un array vació
+     */
     todoList(){
       return this.$store.state.todoList
     },
-
+    /**
+     * Retorna la información completa de una tarea almacenada en el store o un null
+     */
     todoDetails(){
       return this.$store.state.details
     },
 
   },
 
+  /**
+   * Al tener el componente montado se llama al servicio de obtención de tareas
+   */
   mounted(){
     this.$store.dispatch('fetchAll')
   },
 
   methods: {
+    /**
+     * Abre el formulario para crear una nueva tarea
+     */
     openForm(){
       this.isOpenForm = true;
     },
 
+
+    /**
+     * Asigna el valor de los detalles de tarea a la variable para editar y abre el forumlario de edición
+     */
     assignTodo(){
       this.todoEdit = Object.assign({}, this.todoDetails)
       this.openForm();
     },
 
+    /**
+     * Resetea variables a su estado inicial
+     */
     resetVariables(){
       this.taskSelected = null;
       this.todoEdit = null;
       this.$store.commit('setTodoDetails', null);
     },
 
+    /**
+     * Recibe el id emitido desde el componente de tasklist para ser asingnado a la variable tasSelected local
+     * @param {number} task
+     */
     changeTask(task){
       this.taskSelected = task;
     }
